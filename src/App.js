207a38bafbdde4dfd86f95df6b1eaa7100c9af95
Jacob Walton <jawalton6616@gmail.com>
2020-12-18 09:40:05 -0600

    added media query and right column

--- src/App.js
+++ src/App.js
@@ -1,61 +1,68 @@
 import React, { useState, useEffect } from "react";
-import InfoBox from './InfoBox'
-import { MenuItem, Select, FormControl } from '@material-ui/core';
-import './App.css';
+import InfoBox from "./Components/InfoBox";
+import Map from "./Components/Map";
+import { MenuItem, Select, FormControl, Card, CardContent } from "@material-ui/core";
+import "./App.css";
 
 function App() {
-
   const [countries, setCountries] = useState([]);
-  const [country, setCountry] = useState('worldwide')
+  const [country, setCountry] = useState("worldwide");
 
-  useEffect(() =>{
-    const getCountryData = async () =>{
-      await fetch ('https://disease.sh/v3/covid-19/countries')
-      .then((response)=> response.json())
-      .then((data)=>{
-        const countries = data.map((country) => (
-          {
+  useEffect(() => {
+    const getCountryData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
             name: country.country,
             value: country.countryInfo.iso2,
-          }
-        ));
-        setCountries(countries);
-      })
+          }));
+          setCountries(countries);
+        });
     };
     getCountryData();
   }, [countries]);
 
-  const onCountrySelect = (event) =>{
-  const countryCode = event.target.value;
-  setCountry(countryCode)
-  }
+  const onCountrySelect = (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  };
 
   return (
     <div className="App">
-
-      <div className="header">        
-      <h1>COVID-19 Tracker</h1>
-      <FormControl className="Dropdown">
-        <Select variant="outlined" value={country} onChange={onCountrySelect}>
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-        {
-          countries.map(country => (<MenuItem value={country.value}>{country.name}</MenuItem>))
-        }
-        </Select>
-        </FormControl>
-    </div>
-
-        <div className="Stats">
-        <InfoBox title='COVID Cases' cases={47} total={30000}/>
-        <InfoBox title='Recovered' cases={47} total={20000}/>
-        <InfoBox title='Deaths' cases={47} total={20000}/>
+      <div className="appLeft">
+        <div className="header">
+          <h1>COVID-19 Tracker</h1>
+          <FormControl className="Dropdown">
+            <Select
+              variant="outlined"
+              value={country}
+              onChange={onCountrySelect}
+            >
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
         </div>
 
+        <div className="Stats">
+          <InfoBox title="COVID Cases" cases={47} total={30000} />
+          <InfoBox title="Recovered" cases={47} total={20000} />
+          <InfoBox title="Deaths" cases={47} total={20000} />
+        </div>
 
-      {/* {Table} */}
-
-      {/* {Graph} */}
-      {/* Map */}
+        <Map />
+      </div>
+      <Card className="appRight">
+        <CardContent>
+          <h3>Live cases by country</h3>
+          <h3>Worldwide new cases</h3>
+        </CardContent>
+        {/* {Table} */}
+        {/* {Graph} */}
+      </Card>
     </div>
   );
 }
---
+++ src/Components/Map.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Map() {
+    return (
+        <div className="Map">
+            <h2>I AM A MAP</h2>
+        </div>
+    )
+}
+
+export default Map
