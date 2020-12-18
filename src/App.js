86e3346f2a381ee9e3e41e3b5f564ffea180c3f5
Jacob Walton <jawalton6616@gmail.com>
2020-12-18 08:14:44 -0600

    map country options for dropdown'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import React, { useState } from "react";
+import { MenuItem, Select, FormControl } from '@material-ui/core';
 import './App.css';
 
 function App() {
-  return (
-    <div className="app">
-      <h1>COVID 19 Tracker</h1>
 
-      {/* {HEADER} */}
+  const [countries, setCountries] = useState(["USA", "INDIA", "IRELAND"])
+
+  return (
+    <div className="App">
+
+      <div className="header">        
+      <h1>COVID-19 Tracker</h1>
+      <FormControl className="Dropdown">
+        <Select
+        variant="outlined"
+        value="abc"
+        >
+        {
+          countries.map(country => (<MenuItem value={country}>{country}</MenuItem>))
+        }
+
+        </Select>
+        </FormControl>
+    </div>
+
       {/* {Title & Input dropdown} */}
 
       {/* {InfoBoxes} */}
