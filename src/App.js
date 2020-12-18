3388bad34c1cf908366e06c16b34836f78854c52
Jacob Walton <jawalton6616@gmail.com>
2020-12-18 08:30:54 -0600

    fetch from api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MenuItem, Select, FormControl } from '@material-ui/core';
 import './App.css';
 
 function App() {
 
-  const [countries, setCountries] = useState(["USA", "INDIA", "IRELAND"])
+  const [countries, setCountries] = useState([])
+
+  useEffect(() =>{
+    const getCountryData = async () =>{
+      await fetch ('https://disease.sh/v3/covid-19/countries')
+      .then((response)=> response.json())
+      .then((data)=>{
+        const countries = data.map((country) => (
+          {
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }
+        ));
+        setCountries(countries);
+      })
+    };
+    getCountryData();
+  }, [countries]);
 
   return (
     <div className="App">
@@ -17,7 +34,7 @@ function App() {
         value="abc"
         >
         {
-          countries.map(country => (<MenuItem value={country}>{country}</MenuItem>))
+          countries.map(country => (<MenuItem value={country.value}>{country.name}</MenuItem>))
         }
 
         </Select>
