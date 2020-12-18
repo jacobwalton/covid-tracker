4707d4bd44769eb03402a1a1bae638b6e2c221d1
Jacob Walton <jawalton6616@gmail.com>
2020-12-18 07:49:03 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>COVID 19 Tracker</h1>
+
+      {/* {HEADER} */}
+      {/* {Title & Input dropdown} */}
+
+      {/* {InfoBoxes} */}
+      {/* {InfoBoxes} */}
+      {/* {InfoBoxes} */}
+
+      {/* {Table} */}
+
+      {/* {Graph} */}
+      {/* Map */}
+    </div>
+  );
+}
+
+export default App;
